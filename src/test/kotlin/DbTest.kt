f0820de067989add2dc45b0eaafc5c3ad124fc8b
dahamahmed11 <dahamahmed11@gmail.com>
2025-12-10 11:07:16 +0100

    adding the tests

--- src/test/kotlin/DbTest.kt
+++ src/test/kotlin/DbTest.kt
@@ -7,6 +7,7 @@ class DbTest {
     @Test
     fun testDatabaseQuery() {
         val runDb = System.getenv("RUN_DB_TESTS") ?: "0"
+        println("RUN_DB_TESTS = $runDb")
 
         if (runDb == "0") {
             println("Skipping DB tests because RUN_DB_TESTS=0")
@@ -15,6 +16,8 @@ class DbTest {
         }
 
         val name = Database.getUserName(1)
+        println("DB returned: $name")
+
         assertEquals("Ahmed", name)
     }
 }
